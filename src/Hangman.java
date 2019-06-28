2c6abd441dae1296a88ce519f802aaac17d0488a
Solinouk <s.didron@free.fr>
2019-06-28 14:37:21 +0200

    lettre choisie par le joueur. Vérification qu'elle est dans le mot.

---
+++ src/Hangman.java
@@ -0,0 +1,95 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.Random;
+
+
+public class Hangman {
+
+    String word;
+    char[] list_word ;
+    String letter;
+    char good_letter ;
+    Boolean correct ;
+    ArrayList<Integer> index = new ArrayList<Integer>();
+    ArrayList<String> words_list = new ArrayList<String>();
+    String pickWord;
+    int count = 0;
+    Random nb ;
+
+    public Hangman() {
+    }
+
+//    The file where french words 8 letters long are listed in.
+    private File file = new File("/home/solenne/Documents/GITHUB/HANGMAN_JAVA/src/words_8_letters.txt");
+    Scanner sc;
+
+    {
+        try {
+            sc = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public void chose_word() {
+
+        while (sc.hasNext()) {
+            word = sc.next();
+            count += 1;
+            words_list.add(word);
+        }
+        nb = new Random();
+        int n = nb.nextInt(count);
+        pickWord = words_list.get(n);
+        System.out.println(pickWord);
+    }
+
+    public String getPickWord() {
+        return pickWord;
+    }
+
+
+    public void check_letter(char letter, String word) {
+
+//        correct = word.contains(letter);
+
+        for (int i = 0 ;  i < word.length() ; i++) {
+
+            list_word= word.toCharArray();
+
+            if (list_word[i] == letter){
+                good_letter = letter ;
+                index.add(i);
+                correct = true ;
+            }
+            else {
+                System.out.println("Dommage...");
+                correct = false;
+            }
+        }
+    }
+
+    public void display_word(){
+
+
+    }
+
+    public char getGood_letter() {
+        return good_letter;
+    }
+
+    public Boolean getCorrect() {
+        return correct;
+    }
+
+    public ArrayList<Integer> getIndex() {
+        return index;
+    }
+}
+
+
+
+
--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,50 @@
+import java.util.Scanner;
+
 public class Player {
 
-    public Player() {
-        String name ;
-        int score ;
+    String name;
+    int score;
 
+    char let ;
+    char letter ;
+    public Scanner sc = new Scanner(System.in);
+
+
+    public Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public Player() {
+
+    }
+
+    public int get_score(){
+        return score;
+    }
+
+    public String get_name() {
+        return name;
+    }
+
+    public void chose_name(){
+
+        System.out.println("Veuillez saisir votre nom :");
+        name = sc.nextLine();
+        System.out.println("Bienvenue "+ name + " !");
+
+    }
+
+    public void chose_letter(){
+        System.out.println("Veuillez choisir une lettre ");
+        let = sc.next().charAt(0);
+        letter = Character.toUpperCase(let);
+
+        System.out.println("Voyons si la lettre " + letter + " est dans le mot. ");
+
+    }
+
+    public char getLetter() {
+        return letter;
     }
 }
