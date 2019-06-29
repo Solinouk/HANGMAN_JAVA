baf0ed2e0662119470c9200b59010211178e6b72
Solinouk <s.didron@free.fr>
2019-06-29 14:53:53 +0200

    ajout de la fonctionnalité replay

--- src/Player.java
+++ src/Player.java
@@ -4,45 +4,42 @@ public class Player {
 
     String name;
     int score;
-    char let ;
-    char letter ;
+    char let;
+    char letter;
     private Scanner sc = new Scanner(System.in);
 
-    public Player(String name, int score) {
-        this.name = name;
-        this.score = score;
-    }
+//    public Player(String name, int score) {
+//        this.name = name;
+//        this.score = score;
+//    }
 
     public Player() {
 
     }
 
-    public int get_score(){
-        return score;
-    }
 
-    public String get_name() {
-        return name;
-    }
-
-    public void chose_name(){
-
+    //Player chose his name
+    public void chose_name() {
         System.out.println("Veuillez saisir votre nom :");
         name = sc.nextLine();
-        System.out.println("Bienvenue "+ name + " !");
-
+        System.out.println("Bienvenue " + name + " !");
     }
 
-    public void chose_letter(){
+    // he choses a letter
+    public void chose_letter() {
         System.out.println("Veuillez choisir une lettre ");
         let = sc.next().charAt(0);
         letter = Character.toUpperCase(let);
-
         System.out.println("Voyons si la lettre " + letter + " est dans le mot. ");
 
     }
 
     public char getLetter() {
         return letter;
+    }
+
+
+    public String get_name() {
+        return name;
     }
 }
