4bbb6c8aa8123e858eda12cbf27e27844e0aad4c
Solinouk <s.didron@free.fr>
2019-06-29 11:01:29 +0200

    Le programme tourne jusqu'à ce que le mot soit trouvé ou que les essais soient à 0

--- src/Hangman.java
+++ src/Hangman.java
@@ -16,17 +16,20 @@ public class Hangman {
     String pickWord;
     int count = 0;
     Random nb;
+    int tries ;
+    char rejoue;
+    char rejouer ;
 
     public Hangman() {
     }
 
     //    The file where french words 8 letters long are listed in.
     private File file = new File("/home/solenne/Documents/GITHUB/HANGMAN_JAVA/src/words_8_letters.txt");
-    Scanner sc;
+    Scanner scan;
 
     {
         try {
-            sc = new Scanner(file);
+            scan = new Scanner(file);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -35,8 +38,8 @@ public class Hangman {
 
     public void chose_word() {
 
-        while (sc.hasNext()) {
-            word = sc.next();
+        while (scan.hasNext()) {
+            word = scan.next();
             count += 1;
             words_list.add(word);
         }
@@ -52,11 +55,8 @@ public class Hangman {
 
 
     public void check_letter(char letter, String word) {
-
-//        correct = word.contains(letter);
-
+        index.clear();
         for (int i = 0; i < word.length(); i++) {
-
             list_word = word.toCharArray();
 
             if (list_word[i] == letter) {
@@ -65,46 +65,73 @@ public class Hangman {
             }
         }
         if (index.isEmpty()) {
-
             correct = false;
             System.out.println("Dommage...");
 
         } else {
-
             correct = true;
             System.out.println("Bravo !");
-
         }
-
     }
 
-
-    public void display_word(String word, char[] displayed) {
+    public char[] display_word(String word, char[] displayed) {
         System.out.println(index);
         for (int i : index) {
             displayed[i]= good_letter;
-
-
         }
         System.out.println(displayed);
-
-
-
-
+        return displayed;
     }
 
-    public char getGood_letter() {
-        return good_letter;
-    }
 
-    public Boolean getCorrect() {
-        return correct;
-    }
+    public int game_over(String word, char[] displayed, int tries){
 
-    public ArrayList<Integer> getIndex() {
-        return index;
+        correct = getCorrect();//
+
+            if (correct) {
+                System.out.println("bravo, vous avez trouvé une lettre du mot. ");
+                if (Arrays.equals(displayed, word.toCharArray())){
+                    System.out.println(" Vous avez trouvé le bon mot !");
+                    System.out.println("Voulez-vous rejouer ? 'o' ");
+                    rejoue = scan.next().charAt(0);
+                    System.out.println(rejoue);
+                    rejouer = Character.toLowerCase(rejoue);
+                }
+            }
+
+            else{
+                tries -= 1;
+                System.out.println(tries);
+                System.out.println("La lettre n'est pas dans le mot");
+                if (tries == 0) {
+                    System.out.println("Vous avez perdu. Le mot était " + word + ".");
+                    System.out.println("Voulez-vous rejouer ? 'o' ");
+                    rejoue = scan.next().charAt(0);
+                    rejouer = Character.toLowerCase(rejoue);
+
+                }
+                if (tries > 0) {
+                    System.out.println("Il vous reste " + tries + " essais.");
+                }
+            }
+        return tries;
+        }
+
+
+
+    public char getGood_letter() {return good_letter;}
+
+    public Boolean getCorrect() {return correct;}
+
+    public ArrayList<Integer> getIndex() {return index;}
+
+    public char getRejouer() {return rejouer;}
+
+    public int getTries() {
+        return tries;
     }
 }
+
 
 
 
